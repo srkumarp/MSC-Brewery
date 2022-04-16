e5b543b34a420a9aec632950eeabd627d74a5558
srkumarp <srkumarp@USBLRSRKUMARP1.us.deloitte.com>
2022-04-16 19:47:13 +0530

    First commit

---
+++ MSC-Brewery/src/main/java/sriram/springwork/MSCBrewery/MscBreweryApplication.java
@@ -0,0 +1,13 @@
+package sriram.springwork.MSCBrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscBreweryApplication.class, args);
+	}
+
+}
